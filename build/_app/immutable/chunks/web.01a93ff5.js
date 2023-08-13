8d0275804d30c2bbd38c161e7a4864f8d7fe4146
cakePhone <ms8042939@gmail.com>
2023-08-13 01:16:52 +0100

    no ignore build, needed for build from source

---
+++ build/_app/immutable/chunks/web.01a93ff5.js
@@ -0,0 +1,1 @@
+import{W as n}from"./2.bf0baf85.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ build/_app/immutable/nodes/2.c86a8d8e.js
@@ -0,0 +1,1 @@
+import{P as m}from"../chunks/2.bf0baf85.js";export{m as component};
